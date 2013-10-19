3a45df0c5e649d03f3d67bdacfffe56469064e87
nickgrosvenor <rooski@mac.com>
2013-10-18 17:26:39 -0700

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Throoper1::Application.config.secret_key_base = '98a05b926559a1d4991673ee2ea346167232ad1d06650bf78692cd4bf2f505a43e21e0b2242ff224ed4c3f72b333ea7b77e04cd58c535ab06274d0b416fcf572'
